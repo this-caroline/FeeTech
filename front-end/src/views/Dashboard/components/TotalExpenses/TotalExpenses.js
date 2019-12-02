0ac082ba728aa89ab0c1e1bc2f79ab052c88fece
João Wicktor <joaowicktor@gmail.com>
2019-12-02 16:18:49 -0300

    Dashboard

---
+++ front-end/src/views/Dashboard/components/TotalExpenses/TotalExpenses.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { Card, CardContent, Grid, Typography, Avatar } from '@material-ui/core';
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: '100%'
+  },
+  content: {
+    alignItems: 'center',
+    display: 'flex'
+  },
+  title: {
+    fontWeight: 700
+  },
+  avatar: {
+    backgroundColor: theme.palette.error.main,
+    height: 56,
+    width: 56
+  },
+  icon: {
+    height: 32,
+    width: 32
+  },
+  difference: {
+    marginTop: theme.spacing(2),
+    display: 'flex',
+    alignItems: 'center'
+  },
+  differenceIcon: {
+    color: theme.palette.error.dark
+  },
+  differenceValue: {
+    color: theme.palette.error.dark,
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const TotalExpenses = props => {
+  const { className, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <CardContent>
+        <Grid
+          container
+          justify="space-between"
+        >
+          <Grid item>
+            <Typography
+              className={classes.title}
+              color="textSecondary"
+              gutterBottom
+              variant="body2"
+            >
+              DESPESAS
+            </Typography>
+            <Typography variant="h3">R$ 800,00</Typography>
+          </Grid>
+          <Grid item>
+            <Avatar className={classes.avatar}>
+              <RemoveCircleIcon className={classes.icon} />
+            </Avatar>
+          </Grid>
+        </Grid>
+        <div className={classes.difference}>
+          <ArrowDownwardIcon className={classes.differenceIcon} />
+          <Typography
+            className={classes.differenceValue}
+            variant="body2"
+          >
+            12%
+          </Typography>
+          <Typography
+            className={classes.caption}
+            variant="caption"
+          >
+            Desde o último mês
+          </Typography>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+TotalExpenses.propTypes = {
+  className: PropTypes.string
+};
+
+export default TotalExpenses;
---
+++ front-end/src/views/Dashboard/components/index.js
@@ -0,0 +1,7 @@
+export { default as TotalExpenses } from './TotalExpenses';
+export { default as LatestOrders } from './LatestOrders';
+export { default as IncomeExpenseChart } from './IncomeExpenseChart';
+export { default as TasksProgress } from './TasksProgress';
+export { default as TotalBalance } from './TotalBalance';
+export { default as TotalIncome } from './TotalIncome';
+export { default as UsersByDevice } from './UsersByDevice';
